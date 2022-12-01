8e9b2c9a86b6a0d03dbd45f09a3498cb14dc72b9
theirix <theirix@gmail.com>
2022-12-01 08:57:19 +0300

    Initial commit

---
+++ src/day1.rs
@@ -0,0 +1,41 @@
+use crate::{answer, common::Answer};
+
+pub const ANSWER: Answer = answer!(24000, 45000);
+
+pub fn process_a(lines: Vec<String>) -> usize {
+    let mut elfs: Vec<usize> = vec![];
+    let mut elf_calories : usize = 0;
+    for line in lines {
+        if line.is_empty() {
+            elfs.push(elf_calories);
+            elf_calories = 0;
+        } else {
+            elf_calories += line.parse::<usize>().unwrap();
+        }
+    }
+    // last elf
+    if elf_calories > 0 {
+        elfs.push(elf_calories);
+    }
+    println!("{:?}", elfs);
+    *elfs.iter().max().unwrap()
+}
+
+pub fn process_b(lines: Vec<String>) -> usize {
+    let mut elfs: Vec<usize> = vec![];
+    let mut elf_calories : usize = 0;
+    for line in lines {
+        if line.is_empty() {
+            elfs.push(elf_calories);
+            elf_calories = 0;
+        } else {
+            elf_calories += line.parse::<usize>().unwrap();
+        }
+    }
+    // last elf
+    if elf_calories > 0 {
+        elfs.push(elf_calories);
+    }
+    elfs.sort_by(|a,b| b.cmp(a));
+    elfs[0..3].iter().sum()
+}
